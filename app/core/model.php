5a96d916507ec196943e516d6291727c1a262553
dash201 <liamidisad24@gmail.com>
2022-06-16 00:06:01 +0100

    add autoload and update class model

--- app/core/model.php
+++ app/core/model.php
@@ -2,12 +2,14 @@
 namespace core;
 require_once('app/database.php');
 
-class model{
+abstract class model{
     protected $db;
     protected $table;
+    protected $show;
 
     public function __construct(){
        $this->db = $this->conn_db();
+       $this->show = $this->show_fields();
     }
 
     private function conn_db(){
@@ -15,23 +17,50 @@ class model{
         return $pdo;
     }
 
-    public function findAll(){
-        $req = $this->db->query("SELECT * FROM $this->table"); 
-        return $req->fetchAll();
+    public function show_fields(){
+        $req = $this->db->query("Show fields from $this->table");$i = 0; $t="";
+        while($a = $req->fetch()){
+            $i = $i + 1;
+            if($i === sizeof($a)){
+                $t .= $a['Field'];
+            }else{
+                $t .= $a['Field'].',';
+            }
+        }
+        $t = explode(',', $t);
+        array_shift($t);
+        $t = implode(',',$t);
+
+        return $t;
     }
 
-    public function findOder(string $order){
-        $req = $this->db->query("SELECT * FROM $this->table ORDER BY $order"); 
+    public function findAll(?string $order, ?string $limit){
+        if(empty($order) && empty($limit)){
+            $req = $this->db->query("SELECT * FROM $this->table");
+        }elseif(!empty($order) && empty($limit)){
+            $req = $this->db->query("SELECT * FROM $this->table ORDER BY $order"); 
+        }elseif(empty($order) && !empty($limit)){
+            $req = $this->db->query("SELECT * FROM $this->table LIMIT $limit"); 
+        }elseif(!empty($order) && !empty($limit)){
+            $req = $this->db->query("SELECT * FROM $this->table ORDER BY $order LIMIT $limit"); 
+        }
         return $req;
     }
 
-    public function findLimit(string $limit){
-        $req = $this->db->query("SELECT * FROM $this->table LIMIT $limit"); 
+    public function findWhere(string $condition, array $array){
+        $req = $this->db->prepare("SELECT * FROM $this->table WHERE $condition");
+        $req->execute($array);
         return $req;
     }
 
-    public function findOderLimit(string $order, string $limit){
-        $req = $this->db->query("SELECT * FROM $this->table ORDER BY $order LIMIT $limit"); 
-        return $req;
+    public function add(string $value, array $array){
+        $req = $this->db->prepare("INSERT INTO $this->table($this->show) Values ($value)");
+        $req->execute($array);
+        return  $this->db->lastInsertId();
+    }
+
+    public function delete(string $condition, array $array){
+        $req = $this->db->prepare("DELETE FROM $this->table WHERE $condition");
+        $req->execute($array);
     }
 }
